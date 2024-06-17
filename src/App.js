47b5871ecbfc38a3b1bd23ee8c89fced18be4e84
Ryan Dudley <ryguy122000@gmail.com>
2024-06-17 17:55:19 -0400

    inital changes

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+
+const stockdata = [
+  {
+    id : 0,
+    name : "SMCI",
+    hold_range : (875, 900),
+    sell_for_profits : 1000  
+  },
+  {
+    id : 1,
+    name : "RDDT",
+    hold_range : (35,40),
+    sell_for_profits : 67.15  
+  }
+]
+
+
+const Stocks = (stock) => (
+  <ul>
+    {stock.stockitems.map((item) => (
+      <li key = {item.id}>
+        <h2>{item.name}</h2>
+        <p>{item.hold_range}</p>
+        <span>{item.sell_for_profits}</span>
+      </li>
+    ))}
+  </ul>
+)
+const App = () => (
+  <>
+  <Stocks stockitems = {stockdata}/>
+  </>
+)
+
+// root.render(<App/>);
+
+// function App() {
+//   return (
+//     <div className="App">
+//       
+//     </div>
+//   );
+// }
 
 export default App;
